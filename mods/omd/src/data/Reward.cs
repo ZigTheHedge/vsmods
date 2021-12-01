7efe779d6e356a4853358530e3fd7c4415d84565
ZigTheHedge <junk@cwelth.com>
2021-12-01 19:31:33 +0300

    OMD alpha added

---
+++ mods/omd/src/data/Reward.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace omd.src.data
+{
+    public class Reward
+    {
+        public int amount;
+        public string name;
+        public string message;
+    }
+}
