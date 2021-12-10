32cf3130ae7c6a32e21cdd38868c0e98985f471f
ZigTheHedge <junk@cwelth.com>
2021-12-10 16:43:43 +0300

    Added Notification GUI stub

---
+++ mods/omd/src/notifications/EventNotificationGUI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Client;
+
+namespace omd.src.notifications
+{
+    class EventNotificationGUI : HudElement
+    {
+        public EventNotificationGUI(ICoreClientAPI capi) : base(capi)
+        {
+        }
+
+
+        public override void OnRenderGUI(float deltaTime)
+        {
+            base.OnRenderGUI(deltaTime);
+        }
+
+    }
+}
